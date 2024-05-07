6788712cb9b337b616f7965dc700583fcb736784
crpatil1901 <crpatil1901@gmail.com>
2024-05-07 19:54:54 +0530

    Initial Commit

---
+++ tinytorch/kernel.py
@@ -0,0 +1,96 @@
+import math
+
+class Value:
+    def __init__(self, data, _children=(), _op='', label = ''):
+        self.data = data
+        self.grad = 0.0
+        self._backward = lambda: None
+        self._prev = set(_children)
+        self._op = _op
+        self.label = label
+
+    def __repr__(self) -> str:
+        return f"Value(data={self.data})"
+    
+    def __neg__(self):
+        return self * -1
+    
+    def __rmul__(self, other):
+        return self * other
+    
+    def __radd__(self, other):
+        return self + other
+    
+    def __truediv__(self, other):
+        return self * other**-1
+    
+    def __rtruediv__(self, other):
+        return other * self**-1
+    
+    def __sub__(self, other):
+        return self + (-other)
+    
+    def __rsub__(self, other):
+        return other + (-self)
+    
+    def __add__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data + other.data, (self, other), '+')
+        def _backward():
+            self.grad += out.grad
+            other.grad += out.grad
+        out._backward = _backward
+
+        return out
+    
+    def __mul__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data * other.data, (self, other), '*')
+        def _backward():
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
+        out._backward = _backward
+
+        return out
+    
+    def __pow__(self, other):
+        assert isinstance(other, (int, float)), "Only Supports int / float"
+        out = Value(self.data ** other, (self, ), f'**{other}')
+        def _backward():
+            self.grad += other * (self.data ** (other-1)) * out.grad
+        out._backward = _backward
+
+        return out
+     
+    def tanh(self):
+        x = self.data
+        t = (math.exp(2*x) - 1) / (math.exp(2*x) + 1)
+        out = Value(t, (self,), 'tanh')
+        def _backward():
+            self.grad += (1 - t**2) * out.grad
+        out._backward = _backward
+        
+        return out
+    
+    def exp(self):
+        x = self.data
+        e = math.exp(x)
+        out = Value(e, (self,), 'exp')
+        def _backward():
+            self.grad += e * out.grad
+        out._backward = _backward
+        return out
+    
+    def backward(self):
+        topo = []
+        visited = set()
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build_topo(child)
+                topo.append(v)
+        build_topo(self)
+        self.grad = 1.0
+        for node in topo[::-1]:
+            node._backward()
---
+++ tinytorch/net.py
@@ -0,0 +1,52 @@
+from tinytorch.kernel import Value
+import random
+
+class Neuron:
+    def __init__(self, nin):
+        self.w = [Value(random.uniform(-1, 1)) for _ in range(nin)]
+        self.b = Value(random.uniform(-1, 1))
+
+    def __call__(self, x):
+        act = sum((wi*xi for wi, xi in zip(self.w, x)), self.b)
+        out = act.tanh()
+        return out
+    
+    def parameters(self):
+        return self.w + [self.b]
+    
+    def zero_grad(self):
+        for p in self.parameters():
+            p.grad = 0
+    
+class Layer:
+    def __init__(self, nin, nout):
+        self.neurons = [Neuron(nin) for _ in range(nout)]
+
+    def __call__(self, x):
+        outs = [neuron(x) for neuron in self.neurons]
+        return outs[0] if len(outs) == 1 else outs
+    
+    def parameters(self):
+        return [p for neuron in self.neurons for p in neuron.parameters()]
+    
+    def zero_grad(self):
+        for neuron in self.neurons:
+            neuron.zero_grad()
+    
+
+class MLP:
+    def __init__(self, nin, nouts):
+        sz = [nin] + nouts
+        self.layers = [Layer(sz[i], sz[i+1]) for i in range(len(nouts))]
+
+    def __call__(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
+    
+    def parameters(self):
+        return [p for layer in self.layers for p in layer.parameters()]
+    
+    def zero_grad(self):
+        for layer in self.layers:
+            layer.zero_grad()
